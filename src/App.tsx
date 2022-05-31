00e171fbe993724a44b8ed0b101516020ddf8412
Strryke <gavinong2000@gmail.com>
2022-05-31 22:28:39 +0800

    🎬 Quiz card + Reddit api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import QuizCard from "./components/QuizCard";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      hello world
+      <QuizCard />
+    </div>
+  );
 }
 
 export default App;
